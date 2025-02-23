0f840d91cccf6f42eb3674c3e9439a1aace161bb
Raman Kurai <ramankurai27@gmail.com>
2025-02-23 09:56:20 +0530

    Update : SIgnUp endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,32 @@
 const express = require("express")
+const {UserModel , TodoModel} = require("./db")
 
 const app = express()
 
 app.use(express.json())
 
-app.post("/signup" , (req , res)=>{
+app.post("/signup" , async (req , res)=>{
+   const name = req.body.name
+   const email = req.body.email
+   const password = req.body.password
 
+   await UserModel.insert({
+    name : name,
+    email : email,
+    password : password
+   })
+
+   res.json({
+    message : "You are Now Signed Up"
+   })
 })
 
-app.post("/login" , (req , res)=>{
+app.post("/signin" , (req , res)=>{
     
 })
 
 app.post("/todo", (req , res)=>{
-    
+
 })
 
 app.get("/todos" , (req , res)=>{
