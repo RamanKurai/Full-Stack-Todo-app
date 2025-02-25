b9b74ae742123a3ee2dea294be5ed50afdc3ed79
Raman Kurai <ramankurai27@gmail.com>
2025-02-25 23:39:11 +0530

    add : password hashing

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const {auth , JWT_SECRET}  = require("./auth")
+const bcrypt = require('bcrypt');
+
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://ramankurai27:9631WgXmDF52YvSB@cluster0.sh3z1.mongodb.net/todo-app-database"
+  "mongodb+srv://ramankurai27:ToHICv33ei39Lb51@cluster0.sh3z1.mongodb.net/todo-app-database"
 );
 const jwt = require("jsonwebtoken");
 
@@ -16,10 +18,12 @@ app.post("/signup", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const hashedPassword = await bcrypt.hash(password , 5)
+  console.log(hashedPassword)
   await UserModel.create({
     name: name,
     email: email,
-    password: password,
+    password: hashedPassword,
   });
 
   res.json({
@@ -31,12 +35,18 @@ app.post("/signin", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = await UserModel.findOne({
+  const user = await
+   UserModel.findOne({
     email: email,
-    password: password,
   });
-  console.log(user);
-  if (user) {
+  if (!user) {
+    return res.status(403).json({
+     message : "Invalid credentials" 
+    })
+  }
+
+  const passwordMatch = await bcrypt.compare(password , user.password)
+  if (passwordMatch) {
     const token = jwt.sign(
       {
         id: user._id.toString(),
